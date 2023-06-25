c11746fd6ee6bd3374bef4ccc65de65d6f893438
Maaz <abumaaz728@gmail.com>
2023-06-26 01:30:47 +0530

    connected to the MongoDB cloud database

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose');
-
-const Mongo = "mongodb+srv://mohdmaaz64:Abumaaz64@socialmediaapp.r5csei1.mongodb.net/?retryWrites=true&w=majority";
+const Mongo = "mongodb+srv://mohdmaaz64:3915@mydatabase.cxrxlyz.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose.connect(Mongo).then(()=>{
     console.log('Connected to the Database Successfully');
